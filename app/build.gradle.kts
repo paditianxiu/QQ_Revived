66c2244f8c558d870a57fc6432308de0337ea3b4
paditianxiu <paditianxiu@gmail.com>
2026-07-11 15:00:03 +0800

    更新版本号(每次都会忘记)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.padi.qqlite.revived"
         minSdk = 26
         targetSdk = 37
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
     }
 
     buildTypes {
